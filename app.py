1a4829b60cf4be8eadfe94c7fd7c7c5464d137bb
Valeks84 <ao.volkov@gmail.com>
2017-09-20 16:52:00 +0500

    1

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import telebot
-from datetime import datetime
+import datetime
 
 
 TOKEN = '399554838:AAHi0K7ah2skWcRJb7qaqe3NjqYMVzmnZtw'
 
-date = timedate.now()
+date = str(datetime.datetime.now())
 bot = telebot.TeleBot(TOKEN)
 bot.send_message(-1001148320320, text="Обновление от " + date + '\n', parse_mode = 'HTML')
